5d05d666aa73e84f78e2b91106c4b3e0920f3d08
Yusin <sunkatabeckama@gmail.com>
2023-04-08 12:54:14 +0200

    Register user endpoint

---
+++ controllers/user-controller.js
@@ -0,0 +1,17 @@
+const User = require("../models/userModel");
+
+const createUser = async (req, res) => {
+  const email = req.body.email;
+  const findUser = await User.findOne({ email: email });
+
+  if (!findUser) {
+    //Create a new User
+    const newUser = User.create(req.body);
+    res.json(newUser);
+  } else {
+    // User with that email alredy exists
+    res.json({ msg: "User with that email already exists", success: false });
+  }
+};
+
+module.exports = createUser;
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const dbConnection = require('./config/db-connection');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
-app.use('/', (req, res)=>{
-    dbConnection();
-    res.send('Main endpoint working')
-})
+const authRouter = require('./routes/auth-routes')
+
+dbConnection();
+// Middleware for parsing JSON request bodies
+app.use(bodyParser.json()); // Use body-parser.json() for parsing JSON request bodies
+// Middleware for parsing URL-encoded request bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser.urlencoded() for parsing URL-encoded request bodies
+
+app.use('/api/user', authRouter);
+
 app.listen(PORT, ()=>{
     console.log(`Server running at ${PORT}`);
 })
