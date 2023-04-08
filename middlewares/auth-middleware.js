7fbc3a1bd32970c57e112363b1e54a8e6667f522
Yusin <sunkatabeckama@gmail.com>
2023-04-08 18:35:25 +0200

    Creating auth middleware

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,26 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const asyncHandlre = require("express-async-handler");
+
+const authMiddleware = asyncHandlre(async (req, res, next) => {
+  let token;
+  if (req?.headers.authorization?.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+    try {
+      if (token) {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decodedToken?.id);
+        req.user = user;
+        next();
+      }
+    } catch (error) {
+      throw new Error(
+        "Unauthorized! The provided token is either invalid or token has expired, please login again"
+      );
+    }
+  } else {
+    throw new Error("There isn't any token attached to the header");
+  }
+});
+
+module.exports = authMiddleware;
