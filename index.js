637ae3adc20b20480cd277935a3616a54739cb4f
Yusin <sunkatabeckama@gmail.com>
2023-04-08 14:46:51 +0200

    Prettier in work...

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 const app = express();
-const dbConnection = require('./config/db-connection');
-const { errorHandler, notFound } = require('./middlewares/error-handler');
-const dotenv = require('dotenv').config();
+const dbConnection = require("./config/db-connection");
+const { errorHandler, notFound } = require("./middlewares/error-handler");
+const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
-const authRouter = require('./routes/auth-routes')
+const authRouter = require("./routes/auth-routes");
 
 dbConnection();
 // Middleware for parsing JSON request bodies
@@ -13,12 +13,11 @@ app.use(bodyParser.json()); // Use body-parser.json() for parsing JSON request b
 // Middleware for parsing URL-encoded request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser.urlencoded() for parsing URL-encoded request bodies
 
-app.use('/api/user', authRouter);
-
+app.use("/api/user", authRouter);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, ()=>{
-    console.log(`Server running at ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
