375a0467001918a6a6f01067052035de286bf017
Yusin <sunkatabeckama@gmail.com>
2023-04-08 12:54:46 +0200

    File formatting with prettier

--- config/db-connection.js
+++ config/db-connection.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-    console.log('Successfully connected to the database');
+    console.log("Successfully connected to the database");
   } catch (error) {
-    console.log('Error while connecting to the database: ', error);
+    console.log("Error while connecting to the database: ", error);
   }
-}
+};
 
 module.exports = dbConnection;
