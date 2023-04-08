3456e233cf415a4fc58cbdc6e6b60387620c2886
Yusin <sunkatabeckama@gmail.com>
2023-04-08 12:11:06 +0200

    Create db-connection module for MongoDB

---
+++ config/db-connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('Successfully connected to the database');
+  } catch (error) {
+    console.log('Error while connecting to the database: ', error);
+  }
+}
+
+module.exports = dbConnection;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const dbConnection = require('./config/db-connection');
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
-
+app.use('/', (req, res)=>{
+    dbConnection();
+    res.send('Main endpoint working')
+})
 app.listen(PORT, ()=>{
     console.log(`Server running at ${PORT}`);
 })
