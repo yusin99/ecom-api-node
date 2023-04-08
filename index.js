9460f040406a57fdae02c152a16670b474979ada
Yusin <sunkatabeckama@gmail.com>
2023-04-08 21:58:03 +0200

    Installing and initializing cookie-parser package

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const { errorHandler, notFound } = require("./middlewares/error-handler");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/auth-routes");
+const cookieParser = require("cookie-parser")
 
 dbConnection();
 // Middleware for parsing JSON request bodies
 app.use(bodyParser.json()); // Use body-parser.json() for parsing JSON request bodies
 // Middleware for parsing URL-encoded request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser.urlencoded() for parsing URL-encoded request bodies
+app.use(cookieParser());
 
 app.use("/api/user", authRouter);
 
