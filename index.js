97419e1ec56887ccaec661b43a0fb68cf5766124
Yusin <sunkatabeckama@gmail.com>
2023-04-09 18:11:38 +0200

    Install morgan package for logs in console when querying

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const authRouter = require("./routes/auth-routes");
 const productRouter = require("./routes/product-routes");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 dbConnection();
+
+app.use(morgan('combined'));
 // Middleware for parsing JSON request bodies
 app.use(bodyParser.json()); // Use body-parser.json() for parsing JSON request bodies
 // Middleware for parsing URL-encoded request bodies
