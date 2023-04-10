f5e0a6a1b2304e2ce15ec6e8c745c52598f612b6
Yusin <sunkatabeckama@gmail.com>
2023-04-10 19:46:48 +0200

    Update error handling

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,15 +1,29 @@
 const notFound = (req, res, next) => {
-  const error = new Error(`Not Found : ${req.originalUrl}`);
-  res.status(404);
+  const error = new Error(`Not Found: ${req.originalUrl}`);
+  error.statusCode = 404;
   next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode == 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err?.message,
-    stack: err?.stack,
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+  
+  // Create a sanitized copy of the error object
+  const sanitizedError = {
+    message: message,
+    status: statusCode
+  };
+
+  // Log the error with additional information
+  console.error(`Error: ${message}`, {
+    timestamp: new Date(),
+    url: req.url,
+    method: req.method,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+  });
+
+  res.status(statusCode).json({
+    error: sanitizedError
   });
 };
 
