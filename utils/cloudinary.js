7e72579599d4eb77e983f1ed0d8776302f10c12a
Yusin <sunkatabeckama@gmail.com>
2023-04-15 19:13:28 +0200

    Install cloudinary in order to upload our images on the platform + configuring our upload function based on the documentation. Keys and secrets are placed in .env file

---
+++ utils/cloudinary.js
@@ -0,0 +1,23 @@
+const cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
+});
+
+const cloudinaryUploadImg = async (fileToUpload) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(fileToUpload, (result) => {
+      resolve(
+        {
+          url: result.secure_url,
+        },
+        {
+          resource_type: "auto",
+        }
+      );
+    });
+  });
+};
+
+module.exports = { cloudinaryUploadImg };
