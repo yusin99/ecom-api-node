1cde006fba80ce681d341b75332354479b4a3b86
Yusin <sunkatabeckama@gmail.com>
2023-04-15 17:16:56 +0200

    Coupon model

---
+++ models/couponModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+// Declare the Schema of the Mongo model
+var couponSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    uppercase: true,
+  },
+  expire: {
+    type: Date,
+    required: true,
+    unique: true,
+  },
+  discount: {
+    type: Number,
+    required: true,
+  },
+});
+
+//Export the model
+module.exports = mongoose.model("Coupon", couponSchema);
