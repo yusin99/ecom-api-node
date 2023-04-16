ef3517730a5c2980fa843d078fd47e57b53684cc
Yusin <sunkatabeckama@gmail.com>
2023-04-16 19:33:38 +0200

    Order schema

---
+++ models/orderModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+// Declare the Schema of the Mongo model
+var orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+      },
+    ],
+    paymentIntent: {},
+    orderStatus: {
+      type: String,
+      default: "Not processed",
+      enum: [
+        "Not processed",
+        "Processing",
+        "Dispatched",
+        "Calcelled",
+        "Delivered",
+      ],
+    },
+    orderedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+module.exports = mongoose.model("Order", orderSchema);
