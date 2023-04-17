cccdc3385f9a43d81df9329e2f84f8e9536f072f
Yusin <sunkatabeckama@gmail.com>
2023-04-17 23:19:06 +0200

    Commenting the blog-categories controller

--- controllers/blog-category-controller.js
+++ controllers/blog-category-controller.js
@@ -2,6 +2,14 @@ const BCategory = require("../models/blogCategoryModel");
 const asyncHandler = require("express-async-handler");
 const validateMongoDBId = require("../utils/validateMDBId");
 
+/**
+ * @description Creates a new blog category
+ * @route POST /api/blog-category
+ * @access Private
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @returns {json} - JSON object containing the newly created blog category
+ */
 const createCategory = asyncHandler(async (req, res) => {
   try {
     const newCategory = await BCategory.create(req.body);
@@ -11,6 +19,14 @@ const createCategory = asyncHandler(async (req, res) => {
   }
 });
 
+/**
+ * @description Retrieves a single blog category by ID
+ * @route GET /api/blog-category/:id
+ * @access Public
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @returns {json} - JSON object containing the retrieved blog category or an error message
+ */
 const getSingleCategory = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoDBId(id);
@@ -27,6 +43,14 @@ const getSingleCategory = asyncHandler(async (req, res) => {
   }
 });
 
+/**
+ * @description Retrieves all blog categories
+ * @route GET /api/blog-category/all-blog-categories
+ * @access Public
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @returns {json} - JSON object containing the retrieved blog categories or an error message
+ */
 const getAllCategories = asyncHandler(async (req, res) => {
   try {
     const categories = await BCategory.find();
@@ -41,6 +65,15 @@ const getAllCategories = asyncHandler(async (req, res) => {
   }
 });
 
+/**
+ * @description Updates a single blog category by ID
+ * @route PUT /api/blog-category/:id
+ * @access Private
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @returns {json} - JSON object containing the updated blog category or an error message
+ * @throws {Error} - If an error occurs during category update
+ */
 const updateSingleCategory = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoDBId(id);
@@ -58,6 +91,15 @@ const updateSingleCategory = asyncHandler(async (req, res) => {
   }
 });
 
+/**
+ * @description Deletes a single blog category by ID
+ * @route DELETE /api/blog-category/:id
+ * @access Private
+ * @param {object} req - Express request object
+ * @param {object} res - Express response object
+ * @returns {json} - JSON object containing the deleted blog category or an error message
+ * @throws {Error} - If an error occurs during category deletion
+ */
 const deleteSingleCategory = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   validateMongoDBId(id);
@@ -68,6 +110,7 @@ const deleteSingleCategory = asyncHandler(async (req, res, next) => {
     throw new Error(error);
   }
 });
+
 module.exports = {
   createCategory,
   getSingleCategory,
